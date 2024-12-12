67eb3eca2a56a64fb7c55755273a665bfa60fb86
sangamesh patil <sangamesh8309@gmail.com>
2024-12-12 08:36:47 +0530

    complete todo
    To-do list built using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule,ItemComponent],
 })
+
 export class AppComponent {
-  title = 'todo';
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+  addItem(description: string) {
+    if (!description) return;
+  
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
+  
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
 }
---
+++ src/app/item.ts
@@ -0,0 +1,9 @@
+export interface Item{
+    description: string;
+    done: boolean;
+}
+/*the `Item` interface creates an item object model so that your application will understand what an item is.
+ For this to-do list, an `item` is an object that has a description and can be marked done.
+
+
+*/
